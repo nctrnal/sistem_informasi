727c2547c61fc76a5fe653bc3f95b9e69dd0839d
maghfirahnursiam <maghfirahnrsm@gmail.com>
2023-07-29 03:22:58 +0700

    new file:   dosen_pengajar/__pycache__/forms.cpython-311.pyc 	modified:   dosen_pengajar/__pycache__/models.cpython-311.pyc 	modified:   dosen_pengajar/__pycache__/urls.cpython-311.pyc 	modified:   dosen_pengajar/__pycache__/views.cpython-311.pyc 	new file:   dosen_pengajar/forms.py 	new file:   dosen_pengajar/migrations/0001_initial.py 	new file:   dosen_pengajar/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   dosen_pengajar/models.py 	new file:   dosen_pengajar/templates/edit_pengajar.html 	new file:   dosen_pengajar/templates/hapus_pengajar.html 	modified:   dosen_pengajar/templates/pengajar.html 	new file:   dosen_pengajar/templates/tambah_pengajar.html 	modified:   dosen_pengajar/urls.py 	modified:   dosen_pengajar/views.py 	new file:   mahasiswa/migrations/0001_initial.py 	modified:   mahasiswa/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   static/assets/css/MyStyle.css

--- dosen_pengajar/models.py
+++ dosen_pengajar/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+def user_directory_path(instance, filename):
+    # Untuk menyimpan foto dengan nama berdasarkan username pengguna
+    return f'user_{instance.nama}/{filename}'
+
+
+class DosenPengajarModel(models.Model):
+
+    pilihan_status = [
+        ('Aktif', 'Aktif'),
+        ('Tidak Aktif', 'Tidak Aktif')
+    ]
+
+    jenis_kelamin = [
+        ('Laki-Laki', 'L'),
+        ('Perempuan', 'P'),
+    ]
+
+    nrp = models.IntegerField(primary_key=True)
+    foto = models.ImageField(upload_to='uploads/profile/',
+                             default='default.png')
+    nama = models.CharField(max_length=100)
+    jk = models.CharField(max_length=20, choices=jenis_kelamin, default='L')
+    tempat_lahir = models.CharField(max_length=100)
+    tanggal_lahir = models.DateField()
+    alamat = models.CharField(max_length=1000)
+    email = models.CharField(max_length=1000)
+    status = models.CharField(
+        max_length=100, choices=pilihan_status, default='A')
+    prodi = models.CharField(max_length=100)
+    agama = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nama
--- dosen_pengajar/views.py
+++ dosen_pengajar/views.py
@@ -1,8 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import DosenPengajarForm
+from .models import DosenPengajarModel
+from django.contrib import messages
 
 
 def pengajar(request):
     context = {
-        'title': 'Dosen Pengajar'
+        'title': 'Dosen Pengajar',
+        'pengajar_list': DosenPengajarModel.objects.all()
     }
     return render(request, 'pengajar.html', context)
+
+
+def edit_pengajar(request, pk):
+    pengajar = get_object_or_404(DosenPengajarModel, pk=pk)
+
+    if request.method == 'POST':
+        form = DosenPengajarForm(
+            request.POST, request.FILES, instance=pengajar)
+        if form.is_valid():
+            form.save()
+            return redirect('dosen_pengajar')
+    else:
+        form = DosenPengajarForm(instance=pengajar)
+
+    # Konteks untuk halaman edit_mahasiswa.html
+    context = {
+        'title': 'Dosen Pengajar',
+        'form': form,
+    }
+
+    return render(request, 'edit_pengajar.html', context)
+
+
+def hapus_pengajar(request, pk):
+    pengajar = get_object_or_404(DosenPengajarModel, pk=pk)
+
+    if request.method == 'POST':
+        pengajar.delete()
+        messages.success(
+            request, f'Dosen pengajar "{pengajar.nama}" berhasil dihapus.')
+        return redirect('dosen_pengajar')  # Redirect to the 'mahasiswa' view
+
+    return render(request, 'hapus_pengajar.html', {'pengajar': pengajar})
+
+
+def tambah_pengajar(request):
+    if request.method == 'POST':
+        form = DosenPengajarForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Ganti 'daftar_mahasiswa' dengan nama URL untuk halaman daftar mata kuliah
+            return redirect('dosen_pengajar')
+    else:
+        form = DosenPengajarForm()
+
+    # Konteks untuk halaman edit_matkul.html
+    context = {
+        'title': 'Dosen Pengajar',
+        'form': form,
+    }
+    return render(request, 'tambah_pengajar.html', context)
