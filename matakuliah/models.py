c544cde48c2cf1d3894863ca79650f76e365fb8d
Muhammad Najib Al-anshory <muhammadnajib2409@gmail.com>
2023-08-27 05:34:16 +0700

    m

--- matakuliah/models.py
+++ matakuliah/models.py
@@ -15,32 +15,18 @@ class MataKuliah(models.Model):
         ('Matematika Teknik', 'Matematika Teknik'),
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     pilihan_tipe = {
         ("Teori", "Teori"),
         ("Praktikum", "Praktikum")
     }
 
-=======
->>>>>>> e44767ec837788c4ef383bfceb6177d8c04b4d7c
-=======
->>>>>>> e44767ec837788c4ef383bfceb6177d8c04b4d7c
     kode_mata_kuliah = models.CharField(max_length=10)
     nama_mata_kuliah = models.CharField(max_length=100, choices=pilihan_matkul)
     jumlah_sks = models.IntegerField()
     program_studi = models.CharField(max_length=50)
-<<<<<<< HEAD
-<<<<<<< HEAD
     tipe = models.CharField(max_length=100, blank=True)
     kuota = models.IntegerField(blank=True, default=30)
     kelas = models.CharField(max_length=100, default="A")
-=======
-    kelas = models.CharField(max_length=10)
->>>>>>> e44767ec837788c4ef383bfceb6177d8c04b4d7c
-=======
-    kelas = models.CharField(max_length=10)
->>>>>>> e44767ec837788c4ef383bfceb6177d8c04b4d7c
     nama_pengajar = models.ForeignKey(
         DosenPengajarModel, on_delete=models.CASCADE, related_name='mata_kuliah_list')
 
